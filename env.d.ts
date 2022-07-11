69edbb69dd4e3679f3b7784474fcce003ec3940b
Andreas Trolle <andreas.trolle@hotmail.com>
2022-07-11 18:08:06 +0200

    .env vars added to env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,10 @@
 declare namespace NodeJS {
     interface ProcessEnv {
+        DATABASE_URL: string
+        NEXTAUTH_SECRET: string
+        NEXTAUTH_URL: string
         AUTH0_ID: string
         AUTH0_SECRET: string
         AUTH0_ISSUER: string
-        NEXTAUTH_URL: string
     }
   }
