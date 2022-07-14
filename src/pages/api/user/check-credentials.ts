2dcaf46da7e50029bdba310ec49c2ec504e9e54e
Andreas Trolle <andreas.trolle@hotmail.com>
2022-07-15 00:22:45 +0200

    chore: user auth test

--- src/pages/api/user/check-credentials.ts
+++ src/pages/api/user/check-credentials.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../../server/db/client";
 import sha256 from "crypto-js/sha256";
 import { logger } from "../../../../lib/logger";
 import { omit } from "lodash";
-import { User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 
 export default async function handle(
   req: NextApiRequest,
@@ -22,27 +22,29 @@ const hashPassword = (password: string) => {
   return sha256(password).toString();
 };
 
+const select = Prisma.validator<Prisma.UserSelect>()({
+  id: true,
+  name: true,
+  email: true,
+  image: true,
+  password: true
+})
+
 // POST /api/user
 async function handlePOST(res: NextApiResponse, req: NextApiRequest) {
   
-  // const user = await prisma.user.findUnique({
-  //   where: { email: req.body.username },
-  //   select: {
-  //     id: true,
-  //     name: true,
-  //     email: true,
-  //     image: true,
-  //     password: true
-  //   },
-  // });
+  const user = await prisma.user.findUnique({
+    where: { email: req.body.username },
+    select: select
+  });
 
-  // if (user && user.password == hashPassword(req.body.password)) {
-  //   logger.debug("password correct");
-  //   res.json(omit(user, "password"));
-  // } else {
-  //   logger.debug("incorrect credentials");
-  //   res.status(400).end("Invalid credentials");
-  // }
+  if (user && user.password == hashPassword(req.body.password)) {
+    logger.debug("password correct");
+    res.json(omit(user, "password"));
+  } else {
+    logger.debug("incorrect credentials");
+    res.status(400).end("Invalid credentials");
+  }
 
   res.status(400).end("Invalid credentials");
 }
--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,14 +1,5 @@
 import { createRouter } from "./context";
 import { z } from "zod";
-import { Prisma } from "@prisma/client";
-
-const select = Prisma.validator<Prisma.UserSelect>()({
-  id: true,
-  name: true,
-  email: true,
-  image: true,
-  password: true
-})
 
 export const exampleRouter = createRouter()
   .query("hello", {
@@ -26,22 +17,5 @@ export const exampleRouter = createRouter()
   .query("getAll", {
     async resolve({ ctx }) {
       return await ctx.prisma.example.findMany();
-    },
-  })
-  .query("getSingle", {
-    input: z
-    .object({
-      id: z.string(),
-    })
-    .nullish(),
-    async resolve({ ctx, input }) {
-      const user = await ctx.prisma.user.findUnique({
-        where: { id: input?.id },
-        select: select
-      });
-
-      return {
-        greeting: `Hello ${input?.id ?? "world"}`,
-      };
     },
   });
