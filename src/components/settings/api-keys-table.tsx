221aa48b191a31236aa94e484799af0b79bdb6b7
Andreas Trolle <andreas.trolle@hotmail.com>
2022-07-16 22:13:34 +0200

    chore: user auth test

--- src/components/settings/api-keys-table.tsx
+++ src/components/settings/api-keys-table.tsx
@@ -45,80 +45,82 @@ const ApiKeyTable = () => {
         return (<><Text>No API Keys</Text></>);
     }
 
-    return (
-        <>
-            <Heading size='lg' pb={4}>E-conomic</Heading>
-            <Box borderWidth={'1px'} py={'2'} rounded='lg'>
-                <TableContainer>
-                    <Table variant='simple'>
-                        <Thead>
-                            <Tr>
-                                <Th>Api Key</Th>
-                                <Th>Value</Th>
-                                <Th></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <>
-                            {data?.map((apiKey, index) => {
-                                <Tr>
-                                    <Td>{apiKey.key}</Td>
-                                    {/* <Td>
-                                        {updateApiTokens ?
-                                            <Input placeholder='Key here' /> :
-                                            <Text>hej</Text>}
-                                        {false ? <Text color={'red.600'}>Not set</Text> : <>
-                                            {data?.find(x => x.key === apiKey)}
-                                        </>}
-                                    </Td>
-                                    <Td textAlign={'right'}>
-                                        <Button onClick={() => setUpdateAppSecretToken(!updateAppSecretToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
-                                            Update
-                                        </Button>
-                                    </Td> */}
-                                </Tr>
-                            })}
-                            </>
-                            {/* <Tr>
-                                <Td>App Secret Token</Td>
-                                <Td>
-                                    {updateAppSecretToken ? 
-                                    <Input placeholder='Key here' /> :
-                                    <Text>hej</Text>}
-                                    { false ? <Text color={'red.600'}>Not set</Text> : <>
-                                    {data?.find(x => x.key === 'AppSecretToken')?.value}
-                                    </>}
-                                </Td>
-                                <Td textAlign={'right'}>
-                                    <Button onClick={() => setUpdateAppSecretToken(!updateAppSecretToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
-                                        Update
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td>Agreement Grant Token</Td>
-                                <Td >
-                                    {updateAgreementGrantToken ? <Input placeholder='Key here' /> : <Text color={'red.600'}>Not set</Text>}
-                                </Td>
-                                <Td textAlign={'right'}>
-                                    <Button onClick={() => setUpdateAgreementGrantToken(!updateAgreementGrantToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
-                                        Update
-                                    </Button>
-                                </Td>
-                            </Tr> */}
-                        </Tbody>
-                        <Tfoot>
-                            <Tr>
-                                <Th>Api Key</Th>
-                                <Th>Value</Th>
-                                <Th></Th>
-                            </Tr>
-                        </Tfoot>
-                    </Table>
-                </TableContainer>
-            </Box>
-        </>
-    )
+    return (<><Text>Fallback</Text></>)
+
+    // return (
+    //     <>
+    //         <Heading size='lg' pb={4}>E-conomic</Heading>
+    //         <Box borderWidth={'1px'} py={'2'} rounded='lg'>
+    //             <TableContainer>
+    //                 <Table variant='simple'>
+    //                     <Thead>
+    //                         <Tr>
+    //                             <Th>Api Key</Th>
+    //                             <Th>Value</Th>
+    //                             <Th></Th>
+    //                         </Tr>
+    //                     </Thead>
+    //                     <Tbody>
+    //                         <>
+    //                         {data?.map((apiKey, index) => {
+    //                             <Tr>
+    //                                 <Td>{apiKey.key}</Td>
+    //                                 {/* <Td>
+    //                                     {updateApiTokens ?
+    //                                         <Input placeholder='Key here' /> :
+    //                                         <Text>hej</Text>}
+    //                                     {false ? <Text color={'red.600'}>Not set</Text> : <>
+    //                                         {data?.find(x => x.key === apiKey)}
+    //                                     </>}
+    //                                 </Td>
+    //                                 <Td textAlign={'right'}>
+    //                                     <Button onClick={() => setUpdateAppSecretToken(!updateAppSecretToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
+    //                                         Update
+    //                                     </Button>
+    //                                 </Td> */}
+    //                             </Tr>
+    //                         })}
+    //                         </>
+    //                         {/* <Tr>
+    //                             <Td>App Secret Token</Td>
+    //                             <Td>
+    //                                 {updateAppSecretToken ? 
+    //                                 <Input placeholder='Key here' /> :
+    //                                 <Text>hej</Text>}
+    //                                 { false ? <Text color={'red.600'}>Not set</Text> : <>
+    //                                 {data?.find(x => x.key === 'AppSecretToken')?.value}
+    //                                 </>}
+    //                             </Td>
+    //                             <Td textAlign={'right'}>
+    //                                 <Button onClick={() => setUpdateAppSecretToken(!updateAppSecretToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
+    //                                     Update
+    //                                 </Button>
+    //                             </Td>
+    //                         </Tr>
+    //                         <Tr>
+    //                             <Td>Agreement Grant Token</Td>
+    //                             <Td >
+    //                                 {updateAgreementGrantToken ? <Input placeholder='Key here' /> : <Text color={'red.600'}>Not set</Text>}
+    //                             </Td>
+    //                             <Td textAlign={'right'}>
+    //                                 <Button onClick={() => setUpdateAgreementGrantToken(!updateAgreementGrantToken)} bg={'brand.button-bg'} textColor={'brand.button-text'}>
+    //                                     Update
+    //                                 </Button>
+    //                             </Td>
+    //                         </Tr> */}
+    //                     </Tbody>
+    //                     <Tfoot>
+    //                         <Tr>
+    //                             <Th>Api Key</Th>
+    //                             <Th>Value</Th>
+    //                             <Th></Th>
+    //                         </Tr>
+    //                     </Tfoot>
+    //                 </Table>
+    //             </TableContainer>
+    //         </Box>
+    //     </>
+    // )
 }
 
 export default ApiKeyTable;
