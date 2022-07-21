dedc31dddccea37066744873b97fffadacf9dd53
Andreas Trolle <andreas.trolle@hotmail.com>
2022-07-21 16:39:02 +0200

    Added currencies and seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const load = async () => {
+    try {
+        await prisma.currency.deleteMany({});
+        console.log("Deleted currencies");
+
+        await prisma.currency.createMany({
+            data: [
+                {currency: "United States Dollar", abbreviation: "USD"},
+                {currency: "Danish Krone", abbreviation: "DKK"},
+            ]
+        })
+        console.log("Added currencies");
+
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    };
+}
+
+load();
