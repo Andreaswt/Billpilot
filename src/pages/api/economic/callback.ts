210ca60daeed1e0c13e59908e0f5300b8b2ece59
Andreas Trolle <andreas.trolle@hotmail.com>
2022-09-24 18:06:04 +0200

    Integration setup feedback

--- src/pages/api/economic/callback.ts
+++ src/pages/api/economic/callback.ts
@@ -8,15 +8,22 @@ const callback = async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getServerSession(req, res, nextAuthOptions);
 
     if (session) {
-        let organizationId = session.user.organizationId;
-        if (!organizationId) throw new Error("No organization found");
-
-        let agreementGrantToken = req.query["token"]
-        if (!agreementGrantToken || typeof(agreementGrantToken) != 'string') throw new Error("No token received");
-        
-        await saveAgreementGrantToken(agreementGrantToken, organizationId);
-
-        res.redirect("/dashboard/integrations")
+        try {
+            let organizationId = session.user.organizationId;
+            if (!organizationId) throw new Error("No organization found");
+    
+            let agreementGrantToken = req.query["token"]
+            if (!agreementGrantToken || typeof(agreementGrantToken) != 'string') throw new Error("No token received");
+            
+            await saveAgreementGrantToken(agreementGrantToken, organizationId);
+    
+            const successMessage = encodeURIComponent('Economic integration was successfully set up.')
+            res.redirect("/dashboard/integrations?success=true&message=" + successMessage)
+        }
+        catch (e: any) {
+            const errorMessage = encodeURIComponent('Error happened during Economic Integration.')
+            res.redirect("/dashboard/integrations?error=true&message=" + errorMessage)
+        }
     }
 };
 
--- src/pages/api/jira/callback.ts
+++ src/pages/api/jira/callback.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession as getServerSession } from "next-auth";
 import { saveAgreementGrantToken } from "../../../../lib/integrations/e-conomic";
 import { JiraRotateTokenResponse, saveJiraTokens } from "../../../../lib/integrations/jira";
+import { logger } from "../../../../lib/logger";
 import { authOptions as nextAuthOptions } from "../auth/[...nextauth]";
 
 const callback = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,8 +13,13 @@ const callback = async (req: NextApiRequest, res: NextApiResponse) => {
         let organizationId = session.user.organizationId;
         if (!organizationId) throw new Error("No organization found");
 
-        let code = req.query["code"]
-        if (!code || typeof (code) != 'string') throw new Error("No code received");
+                // If Jira authorization was aborted Jira will return an error in the query
+                if (req.query["error"] && req.query["error_description"] && typeof(req.query["error_description"]) === 'string') {
+                    const errorMessage = encodeURIComponent(req.query["error_description"])
+                    res.redirect("/dashboard/integrations?error=true&message=" + errorMessage)
+                }
+
+        if (!req.query["code"] || typeof (req.query["code"]) != 'string') throw new Error("No code received");
 
         // Get access token
         fetch("https://auth.atlassian.com/oauth/token", {
@@ -26,12 +32,17 @@ const callback = async (req: NextApiRequest, res: NextApiResponse) => {
                     grant_type: "authorization_code",
                     client_id: process.env.JIRA_CLIENT_ID,
                     client_secret: process.env.JIRA_CLIENT_SECRET,
-                    code: code,
+                    code: req.query["code"],
                     redirect_uri: process.env.JIRA_CALLBACK_URL
                 }
             )
         })
             .then(async response => {
+                // If Jira reports and error back, redirect to integration page with error parameter, so error can be shown
+                if (!response.ok) {
+                    const errorMessage = encodeURIComponent('Error happened during Jira Integration')
+                    res.redirect("/dashboard/integrations?error=true&message=" + errorMessage)
+                }
                 return response.json() as Promise<JiraRotateTokenResponse>
             })
             .then(data => {
@@ -46,19 +57,19 @@ const callback = async (req: NextApiRequest, res: NextApiResponse) => {
                     .then(async response => {
                         return response.json() as Promise<{ id: string }[]>
                     })
-                    .then(cloudIdResponse => {
+                    .then(async cloudIdResponse => {
                         if (!cloudIdResponse[0].id) throw new Error("Cloud id missing")
                         if (!data.access_token && !data.refresh_token) throw new Error("Token missing")
 
                         const requestUrl = "https://api.atlassian.com/ex/jira/" + cloudIdResponse[0].id
 
                         // Finally save tokens
-                        saveJiraTokens(data, requestUrl, organizationId)
+                        saveJiraTokens(data, requestUrl, organizationId).then(() => {
+                            const successMessage = encodeURIComponent('Jira integration was successfully set up.')
+                            res.redirect("/dashboard/integrations?success=true&message=" + successMessage)
+                        })
                     })
             })
-
-
-        res.redirect("/dashboard/integrations")
     }
 };
 
