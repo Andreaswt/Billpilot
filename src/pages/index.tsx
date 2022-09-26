afbbc2674b37fab84aea5d5dcd8400b2c6b3d7cb
ExpensiveCable <carl.larsen.dk@gmail.com>
2022-09-26 19:17:23 +0100

    Staged changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,6 +54,8 @@ const Home: NextPage = () => {
 
         <ProjectToInvoiceSection/>
 
+        <ProjectToInvoiceSection/>
+
         <BackendShowcaseSection />
 
         <BeforeBillpilotSection />
