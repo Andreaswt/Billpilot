7105dbd03cd5103330f2fb6b2381a4c866ca5587
Andreas Widtfeldt Trolle <atrol21@student.sdu.dk>
2022-10-11 18:59:12 +0200

    Fixed price time items field array layout

--- src/components/dashboard/clients/view/create-invoice-template.tsx
+++ src/components/dashboard/clients/view/create-invoice-template.tsx
@@ -1,6 +1,6 @@
-import { AddIcon, CloseIcon } from '@chakra-ui/icons'
-import { Button, Checkbox, Flex, FormControl, Text, FormErrorMessage, FormLabel, Heading, Icon, Input, Stack, Wrap, HStack, Badge, IconButton } from '@chakra-ui/react'
-import { Card, CardBody, Divider, Select } from '@saas-ui/react'
+import { AddIcon, CloseIcon, MinusIcon } from '@chakra-ui/icons'
+import { Button, Checkbox, Flex, FormControl, Text, FormErrorMessage, FormLabel, Heading, Icon, Input, Stack, Wrap, HStack, Badge, IconButton, InputGroup, InputLeftAddon, InputRightAddon, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from '@chakra-ui/react'
+import { Card, CardBody, Divider, Select, useFieldArray, useForm } from '@saas-ui/react'
 import * as React from 'react'
 import { BsClockFill } from 'react-icons/bs'
 import { FaFileInvoiceDollar } from 'react-icons/fa'
@@ -12,6 +12,30 @@ interface Props {
 
 export const CreateInvoiceTemplate: React.FC<Props> = (props) => {
     const [activated, setActivated] = React.useState(false)
+    const [showTax1, setShowTax1] = React.useState(false)
+
+    const fixedPriceTimeItemsForm = useForm({
+        reValidateMode: "onSubmit",
+        defaultValues: {
+            fixedPriceTimeItems: [{ name: '', amount: 0 }],
+        },
+    });
+
+    // const methods = useFormContext();
+
+    const { register, control, handleSubmit, reset, formState, watch, setValue } = fixedPriceTimeItemsForm
+
+    const {
+        fields,
+        append,
+        remove,
+        update
+    } = useFieldArray({
+        control,
+        name: "fixedPriceTimeItems"
+    });
+
+    const { errors } = formState;
 
     return (
         <>
@@ -110,46 +134,160 @@ export const CreateInvoiceTemplate: React.FC<Props> = (props) => {
                         <Card>
                             <CardBody>
                                 <Stack gap={2}>
-                                    <Flex justifyContent="space-between">
-                                        <FormControl isInvalid={false}>
-                                            <Flex alignItems="end" gap={4}>
-                                                <Icon mb={2} h={6} w={6} as={FaFileInvoiceDollar} />
-                                                <Stack>
-                                                    <FormLabel m={0} fontSize="sm" htmlFor={`title`}>Title</FormLabel>
-                                                    <Input
-                                                        width="sm"
-                                                        id='title'
-                                                        placeholder="Monthly retainer"
-                                                        variant="filled"
-                                                    />
-                                                </Stack>
-                                                <FormErrorMessage>
-                                                    {/* {errors.clientInformation?.name?.message} */}
-                                                </FormErrorMessage>
-                                            </Flex>
-                                        </FormControl>
-                                        <Flex>
-                                            <Flex>
-                                                {/* Amount input here */}
-                                            </Flex>
-                                        <Stack>
-                                            <FormLabel whiteSpace="nowrap" m={0} fontSize="sm" htmlFor={`exportToEconomic`}>Apply tax</FormLabel>
-                                            <Flex h="full" gap={4} alignItems="center">
-                                                <Checkbox
-                                                    id='exportToEconomic'
-                                                    type="checkbox"
-                                                    variant="filled"
-                                                />
-                                                <Icon h={7} w={7} as={HiReceiptTax} />
-                                            </Flex>
-                                        </Stack>
-                                        </Flex>
-                                    </Flex>
+                                    <form onSubmit={() => console.log("he")}>
+                                        {fields.map((item, index) => {
+                                            return (
+                                                <React.Fragment key={item.id}>
+                                                    <Stack gap={2}>
+                                                        <Flex justifyContent="space-between" gap={4}>
+                                                            <FormControl isInvalid={false}>
+                                                                <Flex alignItems="end" gap={4}>
+                                                                    <IconButton mb={0.5} aria-label='Create Fixes Price Time Item' icon={<MinusIcon />} onClick={() => remove(index)} />
+                                                                    <Stack>
+                                                                        <FormLabel fontSize="sm" htmlFor={`fixedPriceTimeItems.${index}.name`}>Name</FormLabel>
+                                                                        <Input
+                                                                            width="sm"
+                                                                            id='name'
+                                                                            placeholder="Monthly retainer"
+                                                                            variant="filled"
+                                                                            {...register(`fixedPriceTimeItems.${index}.name`, {
+                                                                                required: 'Name is required',
+                                                                                minLength: { value: 0, message: 'Name must be defined' },
+                                                                            })}
+                                                                        />
+                                                                    </Stack>
+                                                                    <FormErrorMessage>
+                                                                        {/* {errors.clientInformation?.name?.message} */}
+                                                                    </FormErrorMessage>
+                                                                </Flex>
+                                                            </FormControl>
 
-                                    <Divider orientation="horizontal" />
+                                                            <Flex gap={4}>
+                                                                <FormControl isInvalid={!!errors.fixedPriceTimeItems?.[index]?.amount}>
+                                                                    <FormLabel fontSize="sm" htmlFor={`timeItems.${index}.amount`}>Amount</FormLabel>
+                                                                    <Flex flexDirection="column">
+                                                                        <InputGroup>
+                                                                            <InputLeftAddon>USD</InputLeftAddon>
+                                                                            <NumberInput
+                                                                                id='amount'
+                                                                                placeholder="Enter Amount"
+                                                                                variant="filled">
+                                                                                <NumberInputField
+                                                                                    {...register(`fixedPriceTimeItems.${index}.amount`, {
+                                                                                        valueAsNumber: true,
+                                                                                        required: 'Time is required',
+                                                                                        min: { value: 0, message: "Time must be larger than 0" },
+                                                                                    })} />
+                                                                                <NumberInputStepper>
+                                                                                    <NumberIncrementStepper />
+                                                                                    <NumberDecrementStepper />
+                                                                                </NumberInputStepper>
+                                                                            </NumberInput>
+                                                                        </InputGroup>
+                                                                        <FormErrorMessage>
+                                                                            {errors.fixedPriceTimeItems?.[index]?.amount?.message}
+                                                                        </FormErrorMessage>
+                                                                    </Flex>
+                                                                </FormControl>
+                                                                <Stack>
+                                                                    <FormLabel whiteSpace="nowrap" m={0} fontSize="sm" htmlFor={`exportToEconomic`}>Apply tax</FormLabel>
+                                                                    <Flex h="full" gap={4} alignItems="center">
+                                                                        <Checkbox
+                                                                            id='exportToEconomic'
+                                                                            type="checkbox"
+                                                                            variant="filled"
+                                                                        />
+                                                                        <Icon h={7} w={7} as={HiReceiptTax} />
+                                                                    </Flex>
+                                                                </Stack>
+                                                            </Flex>
+                                                        </Flex>
+                                                    </Stack>
+                                                </React.Fragment>
+                                            );
+                                        })}
+
+                                        {fields.length > 0
+                                            ? <Flex mt={6} gap={10} alignItems="end" justifyContent="end">
+                                                {/* <FormProvider {...fixedPriceTimeItemsForm}>
+                                    <FixedPriceTimeItemsStats />
+                                </FormProvider> */}
+                                            </Flex>
+                                            : null}
+
+                                    </form>
+
+                                    <Divider />
 
                                     <Flex justifyContent="start">
-                                        <Button leftIcon={<AddIcon />} colorScheme='primary'>
+                                        <Button onClick={() => append({ name: '', amount: 0 })} leftIcon={<AddIcon />} colorScheme='primary'>
+                                            Add item
+                                        </Button>
+                                    </Flex>
+                                </Stack>
+                            </CardBody>
+                        </Card>
+                    </Stack>
+
+                    <Stack>
+                        <Flex justifyContent="start">
+                            <Heading color="primary" size="md">Taxes</Heading>
+                        </Flex>
+                        <Card>
+                            <CardBody>
+                                <Stack gap={2}>
+                                    {
+                                        showTax1
+                                            ? <><Flex gap={4} justifyContent="space-between">
+                                                <FormControl isInvalid={false}>
+                                                    <Flex alignItems="end" gap={4}>
+                                                        <Icon mb={2} h={6} w={6} as={FaFileInvoiceDollar} />
+                                                        <Stack>
+                                                            <FormLabel m={0} fontSize="sm" htmlFor={`taxes`}>Taxes</FormLabel>
+                                                            <Input
+                                                                width="sm"
+                                                                id='taxes'
+                                                                placeholder="VAT"
+                                                                variant="filled"
+                                                            />
+                                                        </Stack>
+                                                        <FormErrorMessage>
+                                                            {/* {errors.clientInformation?.name?.message} */}
+                                                        </FormErrorMessage>
+                                                    </Flex>
+                                                </FormControl>
+                                                <Flex>
+                                                    <FormControl>
+                                                        <FormLabel fontSize="sm" htmlFor={`title`}>Amount</FormLabel>
+                                                        <Flex flexDirection="column">
+                                                            <InputGroup>
+
+                                                                <NumberInput
+                                                                    id='amount'
+                                                                    placeholder="Enter Amount"
+                                                                    variant="filled">
+                                                                    <NumberInputField />
+                                                                    <NumberInputStepper>
+                                                                        <NumberIncrementStepper />
+                                                                        <NumberDecrementStepper />
+                                                                    </NumberInputStepper>
+                                                                </NumberInput>
+                                                                <InputRightAddon>%</InputRightAddon>
+                                                            </InputGroup>
+                                                            <FormErrorMessage>
+
+                                                            </FormErrorMessage>
+                                                        </Flex>
+                                                    </FormControl>
+                                                </Flex>
+                                            </Flex>
+                                                <Divider orientation="horizontal" />
+                                            </>
+                                            : null
+                                    }
+
+                                    <Flex justifyContent="start">
+                                        <Button onClick={() => setShowTax1(true)} leftIcon={<AddIcon />} colorScheme='primary'>
                                             Add item
                                         </Button>
                                     </Flex>
